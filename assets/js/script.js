7242fb0c00c46936ba9962b0957551a96808b034
Aiizon <killian.bonneau@iia-formation.fr>
2023-12-19 20:30:30 +0100

    Updated CV button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const screenHeight = window.innerHeight;
-const screenWidth = window.innerWidth;
-document.addEventListener("scroll", (event) => {
+const projects = document.querySelectorAll(".projects article");
+const projectsFilters = document.querySelectorAll(".projects-filters li");
+document.addEventListener("scroll", () => {
         if (document.documentElement.scrollTop > (screenHeight - document.documentElement.scrollTop / 3.5)) {
             document.querySelector(".menu-items").classList.add("menu-items-scroll");
             document.querySelector(".navigation-desktop").classList.add("navigation-desktop-scroll");
@@ -9,3 +10,19 @@ document.addEventListener("scroll", (event) => {
             document.querySelector(".navigation-desktop").classList.remove("navigation-desktop-scroll");
         }
 });
+for (let i = 0; i < projectsFilters.length; i++) {
+    projectsFilters[i].addEventListener("click", (event) => {
+        let filterType = event.currentTarget.dataset.filterType;
+        for (let j = 0; j < projects.length; j++) {
+            projects[j].classList.remove("hidden");
+        }
+        if (filterType !== undefined) {
+            for (let j = 0; j < projects.length; j++) {
+                if (projects[j].dataset.projectCategory !== filterType) {
+                    projects[j].classList.add("hidden");
+                }
+            }
+        }
+
+    });
+}
